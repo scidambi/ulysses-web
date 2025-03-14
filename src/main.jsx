9841ed693255ed6fe42443e6addc6b24d28900d9
Siddarth Cidambi <siddarthcidambi@Siddarths-MacBook-Pro.local>
2025-03-14 16:10:47 -0700

    fix: Add Material-UI theme provider setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react"; import { createRoot } from "react-dom/client"; import { ThemeProvider, createTheme } from "@mui/material/styles"; import CssBaseline from "@mui/material/CssBaseline"; import "./index.css"; import App from "./App.jsx"; const theme = createTheme(); createRoot(document.getElementById("root")).render(<StrictMode><ThemeProvider theme={theme}><CssBaseline /><App /></ThemeProvider></StrictMode>);
